4f650d535b6ffba7da3bfec5a63f7aa5a366b1a9
Alba <albamrus@gmail.com>
2022-12-21 15:41:31 +0100

    real data created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,15 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Crime::factory()->create([
+           'alertName' => 'Thanos ataca Madrid',
+           'description' => 'Thanos va a atacar Madrid con su guante de mielda',
+           'heroesRequired' => 10,
+           'img' => 'https://www.royalcanin.com/es/dogs/breeds/breed-library/billy',
+           'datetime'=> '2022-12-20 14:00:00',
+         ]);
+
         Crime::factory(5)->create();
     }
 }
