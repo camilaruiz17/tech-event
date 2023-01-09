3778470e752985803274c32ceae4b06306f654ad
CarmenCruces <karmelacfer@gmail.com>
2023-01-09 12:27:56 +0100

    testing read ok

--- tests/Feature/CRUDCrimeTest.php
+++ tests/Feature/CRUDCrimeTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Crime;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -15,10 +16,36 @@ class CRUDCrimeTest extends TestCase
      */
     use RefreshDatabase;
 
-    public function test_example()
+/*   public function test_example()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }*/
+
+    public function test_listCrimeAppearInHomeView(){
+        $this->withExceptionHandling();
+
+        $crimes = Crime::factory(2)->create();
+        $crime = $crimes[0];
+        
+        $response = $this->get('/');
+
+        $response->assertSee($crime->alertName);
+
+        $response->assertStatus(200)
+            ->assertViewIs('home');
+    }
+
+    public function test_anCrimeCanBeDeleted(){
+        $this->withExceptionHandling();
+
+        $crime = Crime::factory()->create();
+        $this->assertCount(1, Crime::all());
+
+        $response = $this->delete('deleteCrime', $crime->id);
+    
+        $this->assertCount(0, Crime::all());
+
     }
 }
