06f2a12ffa541ecc13f3ebf2d2a54c848885a939
Alba <albamrus@gmail.com>
2023-01-13 15:12:30 +0100

    edit and create style first attempt

--- resources/views/editCrime.blade.php
+++ resources/views/editCrime.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
 <div class="containerCreate">
-    <div class="row justify-content-center" style="border: solid 3px red">
-        <div class="col-md-8" style="border: solid 3px blue">
-            <h2 class="card-title">Edit an alert</h2>
+    <div class="row justify-content-center">
+        <div class="col-md-8" style="border: solid 3px black">
+            <h2 class="card-title">Edit an Alert</h2>
             <form class="justify-content-center" action="{{ route('updateCrime', $crime->id) }}" method="POST">
             @method('PATCH')
             @csrf
                 <div class="input-group mb-3">
-                    <span class="input-group-text" id="inputGroup-sizing-default">Alert</span>
+                    <span class="input-group-text" style="border: solid 3px purple" id="inputGroup-sizing-default">Alert</span>
                     <input type="text" name="alertName" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" value="{{ $crime->alertName }}">
                 </div>
                 <div class="input-group mb-3">
@@ -29,12 +29,13 @@
                     <input type="text" name="datetime" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" value="{{ $crime->datetime }}">
                 </div>
 
-                <div class="float-right">
-                    <a href="{{ route('home') }}">🔙</a>
-                </div>
-
-                <div class="btnCreate">
-                    <button type="submit" class="btn btn-outline-succes" value="Create">Edit</button>
+                <div class="btnContainer">
+                    <div class="float-right" >
+                        <a class="cancelBtn" href="{{ route('home') }}">CANCEL</a>
+                    </div>
+                    <div class="btnCreate">
+                        <button type="submit" class="editBtn" value="Create">EDIT</button>
+                    </div>
                 </div>
             </form>
         </div>
