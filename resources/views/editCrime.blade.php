18a8e2b36f10a8fa6278eb02121f6bee5cb26b7d
Alba <albamrus@gmail.com>
2023-01-13 15:39:59 +0100

    Edit and create divs fixed

--- resources/views/editCrime.blade.php
+++ resources/views/editCrime.blade.php
@@ -3,43 +3,47 @@
 @section('content')
 <div class="containerCreate">
     <div class="row justify-content-center">
-        <div class="col-md-8" style="border: solid 3px black">
-            <h2 class="card-title">Edit an Alert</h2>
-            <form class="justify-content-center" action="{{ route('updateCrime', $crime->id) }}" method="POST">
-            @method('PATCH')
-            @csrf
-                <div class="input-group mb-3">
-                    <span class="input-group-text" style="border: solid 3px purple" id="inputGroup-sizing-default">Alert</span>
-                    <input type="text" name="alertName" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" value="{{ $crime->alertName }}">
-                </div>
-                <div class="input-group mb-3">
-                    <span class="input-group-text" id="inputGroup-sizing-default">Heroes Required</span>
-                    <input type="text" name="heroesRequired" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" value="{{ $crime->heroesRequired }}">
-                </div>
-                <div class="input-group mb-3">
-                    <span class="input-group-text" id="inputGroup-sizing-default">Description</span>
-                    <input type="text" name="description" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" value="{{ $crime->description }}">
-                </div>
-                <div class="input-group mb-3">
-                    <span class="input-group-text" id="inputGroup-sizing-default">Image</span>
-                    <input type="text" name="img" class="form-control subirFoto" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" value="{{ $crime ->img}}">
-                </div>
-                <div class="input-group mb-3">
-                    <span class="input-group-text" id="inputGroup-sizing-default">Date Time</span>
-                    <input type="text" name="datetime" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" value="{{ $crime->datetime }}">
-                </div>
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    <h2 class="card-title">Edit an Alert</h2>
+                    <form class="justify-content-center" action="{{ route('updateCrime', $crime->id) }}" method="POST">
+                        @method('PATCH')
+                        @csrf
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" style="border: solid 3px purple" id="inputGroup-sizing-default">Alert</span>
+                            <input type="text" name="alertName" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" value="{{ $crime->alertName }}">
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="inputGroup-sizing-default">Heroes Required</span>
+                            <input type="text" name="heroesRequired" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" value="{{ $crime->heroesRequired }}">
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="inputGroup-sizing-default">Description</span>
+                            <input type="text" name="description" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" value="{{ $crime->description }}">
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="inputGroup-sizing-default">Image</span>
+                            <input type="text" name="img" class="form-control subirFoto" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" value="{{ $crime ->img}}">
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="inputGroup-sizing-default">Date Time</span>
+                            <input type="text" name="datetime" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" value="{{ $crime->datetime }}">
+                        </div>
 
-                <div class="btnContainer">
-                    <div class="float-right" >
+                    <div class="btnContainer">
+                        <div class="float-right" >
                         <a class="cancelBtn" href="{{ route('home') }}">CANCEL</a>
                     </div>
-                    <div class="btnCreate">
-                        <button type="submit" class="editBtn" value="Create">EDIT</button>
+                        <div class="btnCreate">
+                            <button type="submit" class="editBtn" value="Create">EDIT</button>
+                        </div>
                     </div>
+                </form>
                 </div>
-            </form>
+            </div>    
         </div>
     </div>
-</div>  
+</div>
 
 @endsection
